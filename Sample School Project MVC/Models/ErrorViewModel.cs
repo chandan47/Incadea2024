2f4fb1a84027d0a5061046be117c1a67586d743a
himanshu sharma <iamonlyhimanshusharma@gmail.com>
2024-02-21 12:01:23 +0530

    project removed

--- Sample School Project MVC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MYSchool.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
