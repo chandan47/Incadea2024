e6717cc689755f2b5862c062fe08e03264d70db7
Rakshit Halijola <rakshit.halijola@incadea.com>
2024-02-21 23:48:37 +0530

    All files added

---
+++ BankSystem_EntityFramework/Customers.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class Customers
+{
+    [Key]
+    public int accno { get; set; }
+    public string name { get; set; }
+    public string address { get; set; }
+    public string mobile { get; set; }
+    public DateTime dob { get; set; }
+    public decimal balance { get; set; }
+}
